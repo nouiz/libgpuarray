f49bb2e32bb9f60f6947baf4051745ee0c0b9287
Arnaud Bergeron <abergeron@gmail.com>
2013-12-10 22:05:27 -0500

    Add support for events that you can wait for.

--- src/taskq.c
+++ src/taskq.c
@@ -4,40 +4,59 @@
 #define TQ_QUIT    0x2
 #define TQ_DONE    0x4
 
+typedef struct _task {
+  TAILQ_ENTRY(task) entry;
+  void (*func)(void *);
+  void *arg;
+} task;
+
+typedef struct _event {
+  pthread_mutex_t m;
+  pthread_cond_t c;
+  volatile int res;
+};
+
 struct _taskq {
   TAILQ_HEAD(, task) l;
   pthread_mutex_t m;
   pthread_cond_t c;
-  int flags;
+  volatile int flags;
 };
 
-static inline void lock(taskq *tq) {
-  int err;
-  err = pthread_mutex_lock(&tq->m);
-  if (err)
-    abort();
-}
+#define lock(tq) do {				\
+    int err;					\
+    err = pthread_mutex_lock(&tq->m);		\
+    if (err)					\
+      abort();					\
+  } while (0)
 
-static inline void unlock(taskq *tq) {
-  int err;
-  err = pthread_mutex_unlock(&tq->m);
-  if (err)
-    abort();
-}
+#define unlock(tq) {				\
+    int err;					\
+    err = pthread_mutex_unlock(&tq->m);		\
+    if (err)					\
+      abort();					\
+  } while (0)
 
-static inline void sleep(taskq *tq) {
-  int err;
-  err = pthread_cond_wait(&tq->c, &tq->m);
-  if (err)
-    abort();
-}
+#define sleep(tq) {				\
+    int err;					\
+    err = pthread_cond_wait(&tq->c, &tq->m);	\
+    if (err)					\
+      abort();					\
+  } while (0)
 
-static inline void wakeup(taskq *tq) {
-  int err;
-  err = pthread_cond_signal(&tq->c);
-  if (err)
-    abort();
-}
+#define wakeup(tq) {				\
+    int err;					\
+    err = pthread_cond_signal(&tq->c);		\
+    if (err)					\
+      abort();					\
+  } while (0)
+
+#define wakeup_all(tq) {			\
+    int err;					\
+    err = pthread_cond_broadcast(&tq->c);	\
+    if (err)					\
+      abort();					\
+  } while (0)
 
 static inline task *get() {
   return malloc(sizeof(task));
@@ -74,15 +93,16 @@ taskq *taskq_create() {
 }
 
 static void taskq_stop(taskq* tq) {
+  int fl;
   lock(tq);
   tq->flags |= TQ_QUIT;
-  if (tq->flags & TQ_RUNNING) {
-    unlock(tq);
+  fl = tq->flags;
+  if (fl & TQ_RUNNING) {
     wakeup(tq);
-    lock(tq);
-    while (!(tq->flags & TQ_DONE)) {
+    while (!(fl & TQ_DONE)) {
       unlock(tq);
       lock(tq);
+      fl = tq->flags;
     }
   }
   unlock(tq);
@@ -116,6 +136,14 @@ int task_add(taskq *tq, void (*fn)(void *), void *arg) {
   return rv;
 }
 
+static void event_task(void *e) {
+  event_post((event *)e, 1);
+}
+
+int task_event(taskq *tq, event *ev) {
+  return taskq_add(tq, event_task, ev);
+}
+
 static task *taskq_next_work(taskq *tq) {
   task *next;
 
@@ -144,4 +172,52 @@ void taskq_process(taskq *tq) {
     (work->func)(work->arg);
     put(work);
   }
+}
+
+event *event_create(void) {
+  int err;
+  event *res = malloc();
+  if (res == NULL)
+    return NULL;
+  res->done = 0;
+
+  err = pthread_mutex_init(&res->m, NULL);
+  if (err) {
+    free(res);
+    return NULL;
+  }
+
+  err = pthread_cond_init(&res->c, NULL);
+  if (err) {
+    pthread_mutex_destroy(&res->m);
+    free(res);
+    return NULL;
+  }
+
+  return res;
+}
+
+void event_destroy(event *ev) {
+  pthread_mutex_destroy(&ev->m);
+  pthread_cond_destroy(&ev->c);
+  free(ev);
+}
+
+int event_wait(event *ev) {
+  lock(ev);
+  while (ev->done == 0)
+    sleep(ev);
+  unlock(ev);
+}
+
+void event_post(event *ev, int val) {
+  if (val == 0) val = 1;
+  lock(ev);
+  ev->done = val;
+  unlock(ev);
+  wakeup_all(ev);
+}
+
+void event_reset(event *ev) {
+  ev->done = 0;
 }
--- src/taskq.h
+++ src/taskq.h
@@ -4,17 +4,19 @@
 #include <pthread.h>
 #include <sys/queue.h>
 
-typedef struct _task {
-  TAILQ_ENTRY(task) entry;
-  void (*func)(void *);
-  void *arg;
-} task;
-
 typedef struct _taskq taskq;
+typedef struct _event event;
 
-taskq *taskq_create();
+taskq *taskq_create(void);
 void taskq_destroy(taskq *);
 int task_add(taskq *, void (*)(void *), void *);
-void taskq_process(taskq *tq);
+int task_event(taskq *, event *);
+void taskq_process(taskq *);
+
+event *event_create(void);
+void event_destroy(event *);
+int event_wait(event *);
+void event_post(event *, int);
+void event_reset(event *);
 
 #endif
